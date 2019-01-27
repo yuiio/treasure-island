e70d75667e7fa64ea9c0ceca7de455d04b9063e5
yuiio <antony.templier@gmail.com>
2019-01-27 16:31:25 +0100

    Game is now a class

--- game.py
+++ game.py
@@ -1,7 +1,9 @@
 from player import Player
-import world, enemies
+import world
 
-intro_text = """
+class Game:
+
+    begin_text = """
         Durant une nuit festive à Port-Royal, vous avez gagné aux jeux une carte
         maritime au fin fond d'une taverne à l'atmosphère si alcolisée que tout
         le monde est saoul avant même d'avoir commandé son premier verre de
@@ -20,65 +22,71 @@ intro_text = """
         ... LE TRÉSOR DE BARBE NOIR !
         """
 
-def play():
-
-    print(intro_text)
-
-    world.create_world()
-    player = Player(world.starting_position)
-
-    last_input = None
-
-    # for damage in [ 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 15, 20]:
-    #     print("{0:>3s}".format(str(damage)), end="")
-    #     for drunkness in range(0, 6):
-    #         print("{0:>3s}".format(str(player.get_damage(damage, drunkness))), end="")
-    #     print("\n")
-
-
-    while player.is_alive() and not player.victory:
-
-        room = world.tile_at(player.x, player.y)
-        room.visited = True
-        print(room.intro_text(player))
-
-        # Le temps passant l'alcolémie du joueur passe
-        if player.drunkness > 0 and last_input != "b":
-            player.drunkness *= 0.85 if player.drunkness>0.5 else 0
-
-        # Voir l'inventaire n'est pas considéré comme agissant dans le jeu
-        if last_input != "i":
-            room.modify_player(player)
-
-        if player.is_alive() and not player.victory:
-            print("Vos points de vie : {}".format(player.hp))
-            if player.drunkness > 0:
-                print("Ébriété : {}".format(round(player.drunkness,2)))
-
-            print("\nChoisir une action:\n")
-            available_actions = room.available_actions()
-            for action in available_actions:
-                print(action)
-
-            correct_input = False
-            while not correct_input:
-                action_input = input('\nAction: ')
-                for action in available_actions:
-                    if action_input == action.hotkey:
-                        player.do_action(action, **action.kwargs)
-                        correct_input = True
-                        last_input = action.hotkey
-                        break
-
-    if not player.is_alive():
-        print("""
+    player_death_text = """
         Vous venez de rejoindre la horde des pirates mort-vivants qui hantent
         cette île maudite qui jadis comme vous avaient eu le rêve fou de quitter
         ce lieu avec le trésor de Barbe Noire...
 
         Le pirate qui repartira vivant est, semble t-il, pas encore né !
-        """)
+        """
+
+    def __init__(self):
+        world.create_world()
+        self.player = Player(world.starting_position)
+        self.last_input = None
+
+        print('[ BEGIN GAME ]')
+        print(Game.begin_text)
+
+    def run(self):
+        while self.player.is_alive() and not self.player.victory:
+            # room est l'endroit où le jouer se situe
+            room = world.tile_at(self.player.x, self.player.y)
+            room.visited = True
+            print('[ INTRO TEXT ]')
+            print(room.intro_text(self.player))
+
+            # Le temps passant l'alcolémie du joueur passe
+            if self.player.drunkness > 0 and self.last_input != "b":
+                self.player.drunkness *= 0.85 if self.player.drunkness>0.5 else 0
+
+            # Voir l'inventaire n'est pas considéré comme agissant dans le jeu
+            if self.last_input != "i":
+                print('[ MODIFY PLAYER ]')
+                room.modify_player(self.player)
+
+            if self.player.is_alive() and not self.player.victory:
+
+                print('[ STATUS ]')
+                print("Vos points de vie : {}".format(self.player.hp))
+                if self.player.drunkness > 0:
+                    print("Ébriété : {}".format(round(self.player.drunkness,2)))
+
+
+                print('[ CHOICE ACTION ]')
+                print("\nChoisir une action:\n")
+                available_actions = room.available_actions()
+                for action in available_actions:
+                    print(action)
+
+                correct_input = False
+                while not correct_input:
+                    action_input = input('\nAction: ')
+                    for action in available_actions:
+                        if action_input == action.hotkey:
+                            print('[ PLAYER DO ACTION RESULT ]')
+                            self.player.do_action(action, **action.kwargs)
+                            correct_input = True
+                            self.last_input = action.hotkey
+                            break
+
+        if not self.player.is_alive():
+            print('[END GAME]')
+            print(Game.end_text)
+
+
 
 
 if __name__ == "__main__":
-    play()
+    game = Game()
+    game.run()
